00271cb6d0bcefeb167b94f1beafe0029a823de3
mooaire4 <89996569+mooaire4@users.noreply.github.com>
2025-03-30 20:25:06 +0000

    Deploying to gh-pages from @ mooaire4/mooaire4.github.io@6b55cdb4c0f690c131a9baf3aa644046d33fa83b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,7 +490,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%61%6D%6F%6F%72%65@%62%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -498,6 +498,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/arielle-ana-moore", "_blank");
         },
       },{
         id: 'social-rss',
